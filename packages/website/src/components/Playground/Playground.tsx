d878380f41f34266ea1b87c6d4b90ea1116ee370
Rafał Krupiński <krupinski.rafal@gmail.com>
2025-09-21 15:34:43 +0200

    docs: playground

--- packages/website/src/components/Playground/Playground.tsx
+++ packages/website/src/components/Playground/Playground.tsx
@@ -1,3 +1,25 @@
 import type { FC } from 'react';
+import clsx from 'clsx';
 
-export const Playground: FC = () => <h1>TODO</h1>;
+import styles from './Playground.module.css';
+
+export const Playground: FC = () => (
+  <section className={styles.playground}>
+    <div className="container">
+      <div className="row">
+        <div className={clsx('col col--12')}>
+          <iframe
+            style={{
+              width: '100%',
+              height: '500px',
+              border: '0',
+              borderRadius: '8px',
+              overflow: 'hidden',
+            }}
+            src="https://stackblitz.com/edit/stan-playground?embed=1&file=App.tsx"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+);
