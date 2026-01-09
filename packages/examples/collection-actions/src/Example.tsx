b59b5f7d7acb0b2563c06b4306cd6c555a67ac6d
Rafał Krupiński <krupinski.rafal@gmail.com>
2026-01-09 19:51:24 +0100

    docs: update collection-actions example

--- packages/examples/collection-actions/src/Example.tsx
+++ packages/examples/collection-actions/src/Example.tsx
@@ -1,10 +1,8 @@
 import { selectorFamily } from "@rkrupinski/stan";
-import {
-  useStanCallback,
-  useStanCtx,
-  useStanValueAsync,
-} from "@rkrupinski/stan/react";
+import { useStanCallback, useStanValueAsync } from "@rkrupinski/stan/react";
 import { FC } from "react";
+
+import styles from "./Example.module.css";
 
 type ItemDetails = {
   result: {
@@ -26,10 +24,7 @@ const details = selectorFamily<Promise<ItemDetails>, string>(
       return res.json();
     },
   {
-    cachePolicy: {
-      type: "lru",
-      maxSize: 4,
-    },
+    cachePolicy: { type: "lru", maxSize: 3 },
   }
 );
 
@@ -46,22 +41,15 @@ const Details: FC<{ uid: string }> = ({ uid }) => {
 };
 
 export function Example() {
-  const { store } = useStanCtx();
-
-  (window as any).__STORE__ = store;
-
   const refreshItem = useStanCallback(({ refresh }) => (uid: string) => {
     refresh(details(uid));
   });
 
   return (
-    <table
-      style={{ tableLayout: "fixed", borderCollapse: "collapse" }}
-      border={1}
-    >
+    <table className={styles.table} border={1}>
       <thead>
         <tr>
-          <th style={{ width: 200, textAlign: "left" }}>Name</th>
+          <th>Name</th>
           <th>Actions</th>
         </tr>
       </thead>
