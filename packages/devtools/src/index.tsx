dce288c2804509a154659d612440e4257d05f1bc
Rafał Krupiński <krupinski.rafal@gmail.com>
2026-02-11 23:01:14 +0100

    feat: themes

--- packages/devtools/src/index.tsx
+++ packages/devtools/src/index.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { StanProvider } from '@rkrupinski/stan/react';
+import { ThemeProvider } from 'next-themes';
 
 import './index.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <StanProvider>
-    <App />
-  </StanProvider>,
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <StanProvider>
+      <App />
+    </StanProvider>
+  </ThemeProvider>,
 );
