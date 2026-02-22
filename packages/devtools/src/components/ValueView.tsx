b093acc4bb8c3e72e21401c19b86576af287ffd9
Rafał Krupiński <krupinski.rafal@gmail.com>
2026-02-22 19:47:12 +0100

    feat: json output styles

--- packages/devtools/src/components/ValueView.tsx
+++ packages/devtools/src/components/ValueView.tsx
@@ -3,6 +3,12 @@ import { JSONTree, type LabelRenderer } from 'react-json-tree';
 import { useSystemTheme } from '@/theme';
 import type { RenderValue } from '@/types';
 
+const theme = {
+  scheme: 'bright',
+  base00: 'transparent',
+  tree: { margin: 0 },
+} as const;
+
 const labelRenderer: LabelRenderer = keyPath =>
   keyPath.length === 0 ? null : keyPath[0];
 
@@ -16,7 +22,7 @@ export const ValueView = memo<{ renderValue: RenderValue }>(
     return (
       <JSONTree
         data={renderValue.value}
-        theme="bright"
+        theme={theme}
         invertTheme={invert}
         labelRenderer={labelRenderer}
       />
